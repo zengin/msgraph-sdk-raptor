fc5b0873a49c6820281453216356fad2562ea6fe
Vincent Biret <vibiret@microsoft.com>
2020-11-11 09:01:26 -0500

    - fixes an issue where ps replace would take parenthesis as regex group

--- azure-pipelines/java-templates/replace-core-reference.ps1
+++ azure-pipelines/java-templates/replace-core-reference.ps1
@@ -1,5 +1,5 @@
 $content = Get-Content build.gradle -Raw;
 $content = $content -replace "api 'com\.microsoft\.graph:microsoft-graph-core:\d\.\d\.\d'", "implementation name: 'msgraph-sdk-java-core'";
 $flatDirRef = "mavenCentral()`r`n    flatDir {`r`n        dirs '$Env:CORE_PATH/build/libs'`r`n    }"
-$content = $content -replace "mavenCentral()", $flatDirRef
+$content = $content -replace "mavenCentral\(\)", $flatDirRef
 Set-Content -Value $content -Path build.gradle
